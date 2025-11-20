bdc3e33068fc15078ac233104798673b56baf0c1
Elena Lee <tbfjs3976@icloud.com>
2025-11-21 00:11:09 +0900

    11/19

--- src/generic/ex4/MethodMain1.java
+++ src/generic/ex4/MethodMain1.java
@@ -3,12 +3,17 @@ package generic.ex4;
 public class MethodMain1 {
 
     public static void main(String[] args) {
-        Integer i = 10;
+        Integer i = 10; // 이 코드를 보고 i 가 Integer 인걸 컴파일러가 추론 가능하다.
         Object object = GenericMethod.objMethod(i);
 
         System.out.println("명시적 타입 인자 전달");
         Integer result = GenericMethod.<Integer>genericMethod(i);
         Integer integerValue = GenericMethod.<Integer>numberMethod(10);
         Double doubleValue = GenericMethod.<Double>numberMethod(20.0);
+
+        System.out.println("타입 추론");
+        Integer result1 = GenericMethod.genericMethod(i);
+        Integer integerValue1 = GenericMethod.numberMethod(10);
+        Double doubleValue1 = GenericMethod.numberMethod(20.0);
     }
 }
